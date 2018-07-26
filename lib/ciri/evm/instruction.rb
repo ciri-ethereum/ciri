a5d6c6f4df28f7ae868adf6b6e3c292db8426e23
jjy <jjyruby@gmail.com>
2018-07-26 23:34:05 +0800

    fix EVM::Instruction#get_op return STOP if position out of code size

--- lib/ciri/evm/instruction.rb
+++ lib/ciri/evm/instruction.rb
@@ -37,8 +37,8 @@ module Ciri
 
       def get_op(pos)
         code_size = (bytes_code || ''.b).size
-        return OP::STOP if pos == code_size
-        return OP::INVALID if pos >= code_size
+        return OP::STOP if pos >= code_size
+        # return OP::INVALID if pos >= code_size
         bytes_code[pos].ord
       end
 
