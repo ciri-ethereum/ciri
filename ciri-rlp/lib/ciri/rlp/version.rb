38a30f464d21a0c618b3323cf1e67385440b753c
jjy <jjyruby@gmail.com>
2018-08-09 18:49:29 +0800

    move ciri-rlp, ciri-utils, ciri-crypto to separate repos

--- ciri-rlp/lib/ciri/rlp/version.rb
+++
@@ -1,5 +0,0 @@
-module Ciri
-  module RLP
-    VERSION = "0.2.1"
-  end
-end
--- ciri-rlp/spec/ciri/rlp_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe Ciri::RLP do
-  it "has a version number" do
-    expect(Ciri::RLP::VERSION).not_to be nil
-  end
-end
